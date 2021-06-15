5a5a8101bdb01c44a5ca5ca2dc9f34c952ab64a7
Abhinav Chebrolu <78219591+AbhinavChebrolu@users.noreply.github.com>
2021-06-15 08:19:12 +0530

    Update Depth First Search.cpp

--- Depth First Search.cpp
+++ Depth First Search.cpp
@@ -53,7 +53,7 @@ void printGraph(struct Graph* graph) {
   int v;
   for (v = 0; v < graph->numVertices; v++) {
     struct node* temp = graph->adjLists[v];
-    printf("\n Adjacency list of vertex %d\n ", v);
+    printf("Adjacency list of vertex %d : ", v);
     while (temp) {
       printf("%d -> ", temp->vertex);
       temp = temp->next;
